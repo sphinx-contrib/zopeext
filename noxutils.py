58bb5f19db69445db64c932eb32a27cb1d62e6d3
Michael McNeil Forbes <michael.forbes+python@gmail.com>
2022-09-03 23:53:35 -0700

    Updated noxutils.py

--- noxutils.py
+++ noxutils.py
@@ -12,19 +12,21 @@ from typing import Optional
 from urllib.request import urlopen, Request
 
 from poetry.core.factory import Factory
-from poetry.core.semver import parse_single_constraint as parse_version
+try:
+    from poetry.core.semver.helpers import parse_single_constraint
+except ModuleNotFoundError:
+    from poetry.core.semver import parse_single_constraint
 
 
-VERSION_PARTS = ("major", "minor", "patch")
+VERSION_PARTS = ('major', 'minor', 'patch')
 
 
-def get_versions(
-    dependency: str,
-    granularity: str = "minor",
-    # ascending: bool = False, limit: Optional[int] = None,
-    # allow_prerelease: bool = False,
-) -> Iterable[str]:
+def get_versions(dependency: str, granularity: str = 'minor',
+                 # ascending: bool = False, limit: Optional[int] = None,
+                 # allow_prerelease: bool = False,
+                 ) -> Iterable[str]:
     """Yield all versions of `dependency` considering version constraints
+
     Args:
         dependency: the name of the dependency
         granularity: yield only the newest patch version of each major/minor
@@ -33,9 +35,11 @@ def get_versions(
             use without the 'limit' arg)
         limit: maximum number of entries to return
         allow_prerelease: whether to include pre-release versions
+
     Yields:
         All versions of `dependency` that match the version constraints defined
         and in this project's pyproject.toml and the given `granularity`.
+
     """
     package = Factory().create_poetry(Path(__file__).parent).package
     for requirement in package.requires:
@@ -43,22 +47,21 @@ def get_versions(
             break
     else:
         raise ValueError(f"{package.name} has no dependency '{dependency}'")
-    filtered_versions = [
-        version
-        for version in all_versions(dependency)
-        if requirement.constraint.allows(version)
-    ]
-    parts = VERSION_PARTS[: VERSION_PARTS.index(granularity) + 1]
+    filtered_versions = [version for version in all_versions(dependency)
+                         if requirement.constraint.allows(version)]
+    parts = VERSION_PARTS[:VERSION_PARTS.index(granularity) + 1]
     result = {}
     for version in filtered_versions:
         key = tuple(getattr(version, part) for part in parts)
-        result[key] = max((result[key], version)) if key in result else version
+        result[key] = (max((result[key], version))
+                       if key in result else version)
     return [str(version) for version in result.values()]
 
 
 def all_versions(dependency):
-    request = Request(f"https://pypi.org/pypi/{dependency}/json")
+    request = Request(f'https://pypi.org/pypi/{dependency}/json')
     response = urlopen(request)
-    json_string = response.read().decode("utf8")
+    json_string = response.read().decode('utf8')
     json_data = json.loads(json_string)
-    yield from (parse_version(version) for version in json_data["releases"])
+    yield from (parse_single_constraint(version)
+                for version in json_data['releases'])
